c04f8794124385e228ca5e341b96b6611457fe61
Michael Klein <mbklein@gmail.com>
2015-04-23 16:38:45 -0500

    Fix spacing in Java source

--- src/org/avalonmediasystem/security/module/AvalonSecurity.java
+++ src/org/avalonmediasystem/security/module/AvalonSecurity.java
@@ -23,69 +23,69 @@ import org.apache.http.NameValuePair;
 
 public class AvalonSecurity extends ModuleBase {
 
-    public String defaultUrl = "http://localhost:3000/";
+  public String defaultUrl = "http://localhost:3000/";
 	private URL baseAuthUrl;
     
-    public void onAppStart(IApplicationInstance appInstance) {
-    	String avalonUrl = appInstance.getProperties().getPropertyStr("avalonUrl",defaultUrl);
-    	try {
-    		baseAuthUrl = new URL(avalonUrl);
-    		getLogger().info("Initialized Avalon security module at " + baseAuthUrl);
-    	} catch(MalformedURLException err) {
-    		getLogger().error("Unable to initialize Avalon security module", err);
-    	}
-    }
+  public void onAppStart(IApplicationInstance appInstance) {
+  	String avalonUrl = appInstance.getProperties().getPropertyStr("avalonUrl",defaultUrl);
+  	try {
+  		baseAuthUrl = new URL(avalonUrl);
+  		getLogger().info("Initialized Avalon security module at " + baseAuthUrl);
+  	} catch(MalformedURLException err) {
+  		getLogger().error("Unable to initialize Avalon security module", err);
+  	}
+  }
     
-    private String authStream(String authToken) {
-    	URL authUrl;
+  private String authStream(String authToken) {
+  	URL authUrl;
 
-    	try {
-            authUrl = new URL(baseAuthUrl, "authorize?token="+authToken);
-        } catch(MalformedURLException err) {
-            getLogger().error("Error parsing URL", err);
-            return null;
-        }
-
-        getLogger().debug("Authorizing against " + authUrl.toString());
-        try {
-            HttpURLConnection http = (HttpURLConnection)authUrl.openConnection();
-            http.addRequestProperty("Accept", "text/plain");
-            http.setRequestMethod("GET");
-            http.connect();
-            if (http.getResponseCode() != 202 ) {
-                return null;
-            } else {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(http.getInputStream()));
-                String authorized = reader.readLine().trim();
-            	getLogger().debug("Authorized to stream " + authorized);
-                return authorized;
-            }
-        } catch (IOException err) {
-            getLogger().error("Error connecting to " + authUrl.toString(), err);
-            return null;
-        }
+  	try {
+      authUrl = new URL(baseAuthUrl, "authorize?token="+authToken);
+    } catch(MalformedURLException err) {
+      getLogger().error("Error parsing URL", err);
+      return null;
     }
 
-    private String getAuthToken(String source) {
-    	String[] parts = source.split("\\?");
-    	String query = parts[parts.length-1];
-    	List<NameValuePair> httpParams = URLEncodedUtils.parse(query,Charset.defaultCharset());
-    	for (NameValuePair param:httpParams) {
-            if (param.getName().equals("token")) {
-            	return param.getValue();
-            }
-    	}
-    	return null;
+    getLogger().debug("Authorizing against " + authUrl.toString());
+    try {
+      HttpURLConnection http = (HttpURLConnection)authUrl.openConnection();
+      http.addRequestProperty("Accept", "text/plain");
+      http.setRequestMethod("GET");
+      http.connect();
+      if (http.getResponseCode() != 202 ) {
+        return null;
+      } else {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(http.getInputStream()));
+        String authorized = reader.readLine().trim();
+      	getLogger().debug("Authorized to stream " + authorized);
+        return authorized;
+      }
+    } catch (IOException err) {
+      getLogger().error("Error connecting to " + authUrl.toString(), err);
+      return null;
     }
-    
-    public void onConnect(IClient client, RequestFunction function,
-			AMFDataList params) {
-    	AMFDataObj connectObj = (AMFDataObj)params.get(2);
-    	String appName = connectObj.get("app").toString();
-    	String authToken = getAuthToken(appName);
-    	String authorized = authStream(authToken);
-    	client.setStreamReadAccess(authorized);
-    }
+  }
+
+  private String getAuthToken(String source) {
+  	String[] parts = source.split("\\?");
+  	String query = parts[parts.length-1];
+  	List<NameValuePair> httpParams = URLEncodedUtils.parse(query,Charset.defaultCharset());
+  	for (NameValuePair param:httpParams) {
+      if (param.getName().equals("token")) {
+      	return param.getValue();
+      }
+  	}
+  	return null;
+  }
+  
+  public void onConnect(IClient client, RequestFunction function,
+		AMFDataList params) {
+  	AMFDataObj connectObj = (AMFDataObj)params.get(2);
+  	String appName = connectObj.get("app").toString();
+  	String authToken = getAuthToken(appName);
+  	String authorized = authStream(authToken);
+  	client.setStreamReadAccess(authorized);
+  }
 
 	public void onHTTPSessionCreate(IHTTPStreamerSession httpSession) {
 		getLogger().info("onHTTPSessionCreate: " + httpSession.getSessionId());
